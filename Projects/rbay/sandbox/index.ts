80993809040a183a8f994f89caaf769ae159b884
Quách Phú Thuận <thuanmobile1111@gmail.com>
2025-06-09 11:21:34 +0700

    explain: Issues with HSET

--- Projects/rbay/sandbox/index.ts
+++ Projects/rbay/sandbox/index.ts
@@ -1,5 +1,19 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    await client.hSet("car2", {
+        color: "red",
+        name: "Toyota",
+        engine: {
+            cylinders: 8 // no errors, save [object Object]
+        },
+        owner: null || '', // null and undefined will type error if no '' (ex: null.toString() with node-redis library)
+        service: undefined || ''
+    });
+
+    const car = await client.hGetAll("car2");
+    console.log(car);
+};
+
 run();
